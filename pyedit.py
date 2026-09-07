12d37f4fe5594e2b6d6e51e0a9806b7cec269841
nishank <nishank_3@outlook.com>
2026-09-07 19:00:17 +0530

    miniedit v0.3

--- pyedit.py
+++ pyedit.py
@@ -3,13 +3,23 @@
 print("Welcome To Miniedit")
 
 file_path = input("enter the filepath of the file you want to edit: ")
-if file_path in ["quit", "q",]:
+if file_path in ["quit", "q", "Q"]:
     exit()
 
 open(file_path)
 
-while True:
-    command = input("please name the command you need to execute")
+opened_file = open(file_path)
 
-    if command in ["quit", "q"]:
+while True:
+    command = input("please name the command you need to execute:")
+
+    if command in ["quit", "q", "Q"]:
         break
+
+    if command in ["read", "r", 'R']:
+
+        print(opened_file.read())
+
+    if command in ["edit", "e", "E"]:
+        while True:
+            line = input(": ")
